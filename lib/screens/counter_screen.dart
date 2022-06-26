070c9a7b6befe8bf2f6fca4de41cdc625b8abd46
Alejandro Guerrero <alejo.jesus.magne@gmail.com>
2022-06-26 15:50:11 -0400

    app counter

--- lib/screens/counter_screen.dart
+++ lib/screens/counter_screen.dart
@@ -10,6 +10,29 @@ class CounterScreen extends StatefulWidget {
 
 class _CounterScreenState extends State<CounterScreen> {
   int counter = 10;
+
+  void increase() {
+    counter++;
+    setState(() {
+      
+    });
+  }
+
+  void decrease() {
+    counter--;
+    setState(() {
+      
+    });
+  }
+
+  void reset() {
+    counter = 0;
+    setState(() {
+      
+    });
+  }
+
+
   @override
   Widget build(BuildContext context) {
     
@@ -34,34 +57,45 @@ class _CounterScreenState extends State<CounterScreen> {
       ),
       
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-      floatingActionButton: const CustomFloatingActions(),
+      floatingActionButton: CustomFloatingActions( increasteFn: increase, decreasteFn: decrease, resetFn: reset, ),
           
     );
   }
 }
 
 class CustomFloatingActions extends StatelessWidget {
+
+  final Function increasteFn;
+  final Function decreasteFn;
+  final Function resetFn;
+
   const CustomFloatingActions({
-    Key? key,
+    
+    Key? key, 
+    required this.increasteFn,
+    required this.decreasteFn,
+    required this.resetFn,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: const [
+      children: [
         /**increment value counter */
         FloatingActionButton(
-          onPressed: null ,
-          child: Icon(Icons.exposure_plus_1_outlined),
+          onPressed: () => increasteFn(),
+          child: const Icon(Icons.exposure_plus_1_outlined),
         ),
+
         FloatingActionButton(
-          onPressed: null ,
-          child: Icon(Icons.restore),
+          onPressed: () => resetFn()  ,
+          child: const Icon(Icons.restore),
         ),
+
         FloatingActionButton(
-          onPressed: null ,
-          child: Icon(Icons.exposure_minus_1_outlined),
+          onPressed: () => decreasteFn() ,
+          child: const Icon(Icons.exposure_minus_1_outlined),
         ),
       ],
     );
